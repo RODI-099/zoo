982c459553386a59dc663764c4a2625c79643e6a
RODI-099 <rodiiy099@gmail.com>
2024-08-17 19:30:08 +0500

    feat: create post api request

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
 import { DogService } from './dog.service';
 
 
@@ -13,10 +13,18 @@ export class DogController {
     }
 
     @Get("greet/:id")
-    public introduce(@Param() params: any, @Query() query: any ): string {
+    public introduce(@Param() params: any, @Query() query: any): string {
         console.log("params:", params);
         console.log("query:", query);
         return this.dogService.introduce();
+        
+    }
+
+    @Post("edit")
+    public modifyDetail(@Body() body: any): string {
+        console.log("body:", body);
+        
+        return this.dogService.modifyDetail();
         
     }
 }
